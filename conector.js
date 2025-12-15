c54cbfb5c1a6ed8205fae65825556369db32d0d2
Neftali Yagua <neftali.yagua@cortex-ia.com.co>
2025-12-15 17:13:56 -0500

    fix: cambiar endpoint a HTTPS (wss) para demo.relay.coderic.net
    - Actualizar todos los ejemplos de http:// a https://
    - Socket.io usará wss:// automáticamente con HTTPS
    - Corregir conexión WebSocket segura

--- conector.js
+++ conector.js
@@ -3,7 +3,7 @@
  * Cliente JavaScript para conectarse al gateway Relay
  * 
  * Uso:
- *   const relay = new RelayConector('http://localhost:5000');
+ *   const relay = new RelayConector('https://demo.relay.coderic.net');
  *   await relay.conectar();
  *   await relay.identificar('miUsuario');
  *   relay.enviarATodos({ mensaje: 'Hola!' });
@@ -11,7 +11,7 @@
  */
 
 class RelayConector {
-  constructor(url = 'http://demo.relay.coderic.net') {
+  constructor(url = 'https://demo.relay.coderic.net') {
     this.url = url;
     this.socket = null;
     this.usuario = null;
